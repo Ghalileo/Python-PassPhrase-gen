93397d781c7939ca2e0605409867272d217efbb4
Promofinder2 <olivecaus@gmail.com>
2021-11-15 00:37:01 -0600

    started frontend app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,13 @@ const App = () => {
   const [title, setTitle] = useState('');
   
   const [phrases, setPhrases] = useState('');
-  
+
 
 
   return (
     <div className="App">
       <h1>Passphrase </h1> 
-      <button>Deez nuts me!</button>
+      <button className= "btn btn-outline-danger">Deez  me!</button>
       
       <input/>
     </div>
