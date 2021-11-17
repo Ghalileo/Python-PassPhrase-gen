11bd17a395703132881ab2dec6c294c09b02104a
Promofinder2 <olivecaus@gmail.com>
2021-11-17 02:27:51 +0000

    Mongo Address!

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ import asyncio
 
 pp = passPhrase.main()
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://dbUser:UtCgMNghTUiNyKgM@myfirstcluster.py8c0.mongodb.net/MyFirstCluster')
 database = client.PhraseList
 collection = database.phrase
 
