68decaf2e34ca1726702f9e722333b9c06d1a488
ose Okogbo <ose.okogbo@gmail.com>
2021-12-06 21:50:03 -0600

    moved copy of app for tutorial purpose

--- frontend/src/App copy.js
+++ frontend/src/App copy.js
@@ -13,6 +13,12 @@ const App = () => {
   const [title, setTitle] = useState('');
   const [phrases, setPhrases] = useState('');
 
+  // State objects for the signup data
+  const [signupList, setSignupList] = useState([{}]);
+  const [fullname, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   // Read all phrases
  
 
@@ -30,6 +36,29 @@ const addToHandler = () => {
   .then(res => console.log(res))
 }
 
+// Post User Sign up information
+const signUpHandler = () => {
+  axios.post('http://127.0.0.1:8000/api/user_signup/', {'fullname': fullname, 'email': email, 'password': password})
+  .then(res => console.log(res))
+}
+
+
+
+
+// Read all users
+
+
+useEffect(() => {
+axios.get('http://127.0.0.1:8000/api/user_signup/')
+  .then(res => {
+    setSignupList(res.data)
+  })
+});
+
+
+
+
+
 // Main body of application
   return (
     <div className="App">
@@ -42,6 +71,23 @@ const addToHandler = () => {
           <button className="btn btn-outline-danger" onClick={addToHandler}>Fire!</button>
         </span>
         <br/>
+
+        <div className="App_Login">
+      <h1>Sign Up</h1> 
+      <div className="card-body">
+        <h4>It will be fun</h4>
+        <span className="card-text">
+          <input className="mb-2 form-control titleIn" onChange={event => setFullName(event.target.value)} placeholder="Full Name"/>
+          <input className="mb-2 form-control titleIn" onChange={event => setEmail(event.target.value)} placeholder="Email Address"/>
+          <input className="mb-2 form-control titleIn" onChange={event => setPassword(event.target.value)} placeholder="Password"/>
+          <button className="btn btn-outline-danger" onClick={signUpHandler}>Sign Up</button>
+        </span>
+      </div>
+      </div>
+
+
+        
+
         <br/>
         <br/>
         <h5 className="card text-white bg-dark mb-3">Your Phrases</h5>
