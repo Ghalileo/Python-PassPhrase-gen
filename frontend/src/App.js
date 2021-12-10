f9a06f9c693b920224c60bf49cdbaea6e8552881
Promofinder2 <olivecaus@gmail.com>
2021-12-10 17:29:21 -0600

    UPDATE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,43 +6,8 @@ import POSTPhrase from './components/PostPhrase/postPhrase';
 import GETPhrases from './components/GetPhrases/getPhrases';
 import GETSignups from './components/GetSignups/getSignups';
 const App = () => {
-<<<<<<< HEAD
-  // State object for the title of passphases
-
-  const [phraseList, setPhraseList] = useState([{}]);
-  const [title, setTitle] = useState('');
-  const [phrases, setPhrases] = useState('');
-
-  // State objects for the signup data
-  const [signupList, setSignupList] = useState([{}]);
-
-  // Read all phrases
 
 
- useEffect(() => {
-  axios.get('http://127.0.0.1:8000/api/phrase/')
-    .then(res => {
-      setPhraseList(res.data)
-    })
-});
-// Post a phrase
-const addToHandler = () => {
-  axios.post('http://127.0.0.1:8000/api/phrase/', {'title': title, 'phrases': phrases})
-  .then(res => console.log(res))
-}
-// Read all users
-useEffect(() => {
-axios.get('http://127.0.0.1:8000/api/user_signup/')
-  .then(res => {
-    setSignupList(res.data)
-  })
-});
-
-// Main body of application
-=======
-
-
->>>>>>> d3b6677b25331507a6f767036f5628725f70ee0c
   return (
     <div className="App">
       <Navigation/>
