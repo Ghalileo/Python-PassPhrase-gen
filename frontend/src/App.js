de1dd405363a4a6868c4f9c2eb4b445ba95bb27f
ose Okogbo <ose.okogbo@gmail.com>
2021-12-11 18:26:21 -0600

    cleaner code on app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,28 +18,25 @@ const App = () => {
       <div className="card-body">
         <h4>Add a phrase</h4>
         <div>
-          <POSTPhrase/>
-          
+          <POSTPhrase/> 
         </div>
         <br/>
         <h5 className="card text-white bg-dark mb-3">Your Phrases</h5>
         <div>        
-          <GETPhrases/>
-          
-          </div>
+          <GETPhrases/>  
+        </div>
         <br/>
         <div className="App_Signup">
-      <h1>Sign Up</h1> 
-      <div className="card-body">
-        <h4>It will be fun</h4>
-      </div>
-      </div>
+          <h1>Sign Up</h1> 
+          <div className="card-body">
+            <h4>It will be fun</h4>
+          </div>
+        </div>
         <br/>
-      <h5 className="card text-white bg-dark mb-3">Signed-Up Users</h5>
-      <div>
-        <GETSignups/>
-       
-      </div>
+        <h5 className="card text-white bg-dark mb-3">Signed-Up Users</h5>
+        <div>
+          <GETSignups/>
+        </div>
       </div>
       <h6>Copyright 2021, All rights reserved &copy;</h6>
     </div>
