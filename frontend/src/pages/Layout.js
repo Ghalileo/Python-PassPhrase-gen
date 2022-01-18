1b69873fbac1bc9435036d684dd445f63f3aaa2d
Ghalileo <ose.okogbo@gmail.com>
2022-01-17 23:33:02 -0600

    commented out additional components not in present use

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import MainAppLayout from '../components/layout/layout'
-import POSTPhrase from '../components/PostPhrase/postPhrase'
-import GETPhrases from '../components/GetPhrases/getPhrases'
-import GETSignups from '../components/GetSignups/getSignups'
-import Navigation from '../components/Nav'
+// import POSTPhrase from '../components/PostPhrase/postPhrase'
+// import GETPhrases from '../components/GetPhrases/getPhrases'
+// import GETSignups from '../components/GetSignups/getSignups'
+// import Navigation from '../components/Nav'
 
 const AppLayout = () => {
     // Original Return
